f7186d68734fb59c6d16313be2494161345e7e27
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2020-01-13 11:46:45 +0100

    Ejer1finalizado

--- Ejer6_AndroidRelations/MainActivity.cs
+++ Ejer6_AndroidRelations/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using AlertDialog = Android.App.AlertDialog;
 
 namespace Ejer6_AndroidRelations
 {
@@ -13,24 +14,40 @@ namespace Ejer6_AndroidRelations
     public class MainActivity : AppCompatActivity
     {
 
+        private Button _button1;
+        private EditText _editText1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            /*Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);*/
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+            AddBindForLayout();
         }
+        private void AddBindForLayout()
+        {
+            _button1 = FindViewById<Button>(Resource.Id.buttonInsert);
+            _editText1 = FindViewById<EditText>(Resource.Id.textInsert);
 
+            _button1.Click += pruebecita;
+        }
         public override bool OnCreateOptionsMenu(IMenu menu)
         {
             MenuInflater.Inflate(Resource.Menu.menu_main, menu);
             return true;
         }
 
+        private void pruebecita(object sender, EventArgs e)
+        {
+            AlertDialog.Builder ventanaEmergente = new AlertDialog.Builder(this);
+            ventanaEmergente.SetTitle("Ejercicio 1");
+            ventanaEmergente.SetMessage(_editText1.Text.ToString());
+            ventanaEmergente.Show();
+        }
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
@@ -38,16 +55,13 @@ namespace Ejer6_AndroidRelations
             {
                 return true;
             }
-
             return base.OnOptionsItemSelected(item);
         }
-
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
+            View view = (View)sender;
             Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
-	}
+    }
 }
-
